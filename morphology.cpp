5542e44642ef2521859644a091eae540d5011cbb
NvRom <sa515085@mail.ustc.edu.cn>
2016-08-22 17:32:15 +0800

    腐蚀操作代码实现
    定义了4个函数，实现对图像的腐蚀操作，但没有对性能进行优化

---
+++ morphology.cpp
@@ -0,0 +1,117 @@
+#include "morphology.h"
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+cv::Point getNormalAnchor(cv::Size size , cv::Point anchor){
+	if (anchor.x == -1){
+		anchor.x = size.width / 2;
+	}if (anchor.y == -1){
+		anchor.y = size.height /2;
+	}
+	CV_Assert(anchor.inside(cv::Rect(0 , 0 , size.width , size.height)));
+	return anchor;
+}
+
+bool SEMatchSrc(cv::Mat src , int op , int rowIndex , int colIndex , cv::Mat kernel , cv::Point anchor){
+	CV_Assert(anchor.inside(cv::Rect(0 , 0 , kernel.cols , kernel.rows)));
+	if (op == EROSION){
+		for (int i = 0 ; i < kernel.rows ; i ++){
+			for (int j = 0 ; j < kernel.cols ; j ++){
+				if((kernel.at<uchar>(i , j) == 0 && src.at<uchar>(rowIndex + i - anchor.x , colIndex + j - anchor.y) == 0)
+					|| (kernel.at<uchar>(i , j) == 1 && src.at<uchar>(rowIndex + i - anchor.x , colIndex + j - anchor.y) == 255)){
+						continue;
+				}else
+					return false;
+			}
+		}
+		return true;
+	}
+	if (op == DILATION){
+		return false;
+	}
+}
+
+cv::Mat getSE(int shape , cv::Size size , cv::Point anchor){
+	int ellopse_x , ellopse_y;
+	double ellopse_r = 0;//椭圆形的圆心
+	int i ,j;//循环计数，
+	CV_Assert(shape == MOR_RECT || shape == MOR_CROSS || shape == MOR_ELLOPSE);
+	if (size == cv::Size(1,1)){
+		shape = MOR_RECT;
+	}
+	if (shape == MOR_ELLOPSE){
+		ellopse_x = size.width / 2;
+		ellopse_y = size.height / 2;
+		ellopse_r = ellopse_y ? static_cast<double>(1 / (ellopse_y * ellopse_y)) :0;
+	}
+	anchor = getNormalAnchor(size , anchor);
+	cv::Mat elem(size , CV_8U);
+	for (i  = 0 ; i < size.height ; i ++){
+		//j0,j1表示从外部进入该shape以及从shape出去时，x轴坐标的值，两个可确定shape的边界
+		//j0表示该下标为1
+		int j0 = 0 , j1 = 0;
+		uchar *ptr = elem.ptr(i);
+
+		if(shape == MOR_RECT || (shape == MOR_CROSS && i == anchor.y))//矩形区域计算
+			j1 = size.width;
+		else if (shape = MOR_CROSS){//十字区域计算
+			j0 = anchor.x;
+			j1 = j0 + 1;
+		}else{//椭圆形区域计算
+			int dy = std::abs(ellopse_y - i);
+			int dx = static_cast<int>(ellopse_x * std::sqrt((ellopse_y * ellopse_y - dy * dy)*ellopse_r));
+			j0 = std::max(0 , ellopse_x - dx);
+			j1 = std::min(size.width , ellopse_x + size.width);
+		}
+		//赋值
+		for (j = 0 ; j < j0 ; j ++){
+			ptr[j] = 0;
+		}
+		for (; j < j1 ; j ++){
+			ptr[j] = 1;
+		}
+		for (; j < size.width ; j ++){
+			ptr[j] = 0;
+		}
+	}
+	return elem;
+
+}
+
+void erosion(cv::Mat src , cv::Mat &dst , cv::Mat kernel , cv::Point anchor){
+	CV_Assert(src.size() == dst.size());
+	if (kernel.empty()){
+		kernel = getSE(MOR_RECT , cv::Size(3,3) , anchor);
+	}
+	cv::Size size = kernel.size();
+	if (anchor == cv::Point(-1,-1)){
+		anchor = getNormalAnchor(size ,anchor);
+	}
+	//腐蚀操作，图像结果会收缩
+	int topMargin = anchor.y;
+	int downMargin = size.height - anchor.y - 1;
+	int leftMargin = anchor.x;
+	int rightMargin = size.width - anchor.x - 1;
+
+	for (int i = topMargin ; i < src.rows - downMargin ; i ++){
+		for (int j = leftMargin ; j < src.cols - rightMargin ; j ++){
+			if (SEMatchSrc(src , EROSION , i , j , kernel , anchor)){
+				dst.at<uchar>(i,j) = 255;//匹配上，置255（白色）
+			}else{
+				dst.at<uchar>(i,j) = 0;//未匹配上，置0（黑色）
+			}
+		}
+	}
+	//边缘配色
+	for (int i = 0 ; i < src.rows ; i ++){
+		for (int j = 0 ; j < src.cols ;j ++){
+			if (i < topMargin || i >= src.rows - downMargin){
+				dst.at<uchar>(i,j) = 0;
+			}else if (j < leftMargin || j >= src.cols - rightMargin){
+				dst.at<uchar>(i,j) = 0;
+			}
+		}
+	}
+
+}
---
+++ morphology.h
@@ -0,0 +1,46 @@
+#include<opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+//分别表示为矩形，十字，椭圆
+enum morShape{
+	MOR_RECT , MOR_CROSS , MOR_ELLOPSE
+};
+//形态学基本操作
+enum  morOp{
+	EROSION , DILATION
+};
+
+/*
+得到指定大小和规模的结构体
+@param shape:结构体的形状，有矩形、十字形、椭圆形。若没指定形状，默认为矩形
+@param size:结构体的大小，若没指定大小，默认为3*3
+@param anchor:结构体的原点位置，默认anchor为(-1,-1),表示结构体的中心
+*/
+cv::Mat getSE(int shape , cv::Size size , cv::Point anchor = cv::Point(-1,-1));
+
+/*
+若结构体原点是默认，则返回中心点
+此函数被getSE函数调用
+@param size:结构体的大小
+@param anchor:结构体的原点位置
+*/
+cv::Point getNormalAnchor(cv::Size size , cv::Point anchor);
+
+/*
+图像的腐蚀操作
+@param src：输入图像，要求是灰度图
+@param dst：输出图像，大小和类型与src一样
+@param kernel：结构体（SE），可以通过getSE函数得到，提供以下几种kernel，分别为：MOR_RECT , MOR_CROSS , MOR_ELLOPSE
+@param anchor：结构体的原点，默认anchor为（-1，-1）表示原点位于结构体的中心位置
+*/
+void erosion(cv::Mat src , cv::Mat &dst , cv::Mat kernel , cv::Point anchor = cv::Point(-1,-1));
+/*
+判断结构体与原图像上是否匹配，匹配返回true，否则返回false
+@param src：输入图像，二值化的图像
+@param op：操作类型，有腐蚀（erosion）和膨胀（dilation）两种基本操作
+@param (rowIndex,colIndex)：kernel原点所在图像的位置
+@param kernel：结构体（SE），可以通过getSE函数得到，提供以下几种kernel，分别为：MOR_RECT , MOR_CROSS , MOR_ELLOPSE
+@param anchor：结构体的原点，默认anchor为（-1，-1）表示原点位于结构体的中心位置
+*/
+bool SEMatchSrc(cv::Mat src , int op , int rowIndex , int colIndex , cv::Mat kernel , cv::Point anchor);
